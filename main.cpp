d5d2def103da581f452579203683b8a80f3086e3
zane <zane@yun.io>
2013-11-11 10:47:39 +0800

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include "mainwindow.h"
+#include <QApplication>
+#include "logindialog.h"
+#include <QDebug>
+#include <QStyleFactory>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    //QApplication::setStyle("Cleanlooks");
+    qDebug() << QStyleFactory::keys();
+    MainWindow w;
+    LoginDialog dlg;
+    if (dlg.exec() == QDialog::Accepted) {
+        w.move(dlg.pos());
+        w.show();
+        return a.exec();
+    } else {
+        return 0;
+    }
+}
